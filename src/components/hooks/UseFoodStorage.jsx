403ec0ac5416dda2dac193e1b49f72f7ec677a11
ETorresSacha <eriktorressacha@gmail.com>
2023-11-02 19:33:37 -0500

    hook

---
+++ src/components/hooks/UseFoodStorage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const UseFoodStorage = () => {
+  const handleSaveFood = () => {};
+
+  const handleGetFoods = () => {};
+  return { onSaveFood: handleSaveFood, onGetFood: handleGetFoods };
+};
+
+// Guardar informacion de comida
+// Metodo para obtener info de comida
+
+export default UseFoodStorage;
